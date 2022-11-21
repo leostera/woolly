0435c22bfaee1a66474c111aa709f39d730c30f1
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-21 09:16:15 +0100

    fix: import jwk key from environment

--- api/routes/api/jwt.ts
+++ api/routes/api/jwt.ts
@@ -1,9 +1,9 @@
 import * as JWT from "https://deno.land/x/djwt@v2.8/mod.ts";
 
-const KEY = await crypto.subtle.generateKey(
-  { name: "HMAC", hash: "SHA-512" },
-  true,
-  ["sign", "verify"],
+const _jwk_key = JSON.parse(Deno.env.get("WOOLLY_JWT_SIGN_KEY"));
+
+const KEY = await crypto.subtle.importKey(
+  "jwk", _jwk_key, { name: "HMAC", hash: "SHA-512" }, true, ["sign", "verify"]
 );
 
 const encode = async (payload) => {
