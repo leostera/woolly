2c5dd549e33996bc989ee897ec55400e5411cad4
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-21 06:11:25 +0100

    try using layout effect to force this to run on render

--- api/islands/SaveUserData.tsx
+++ api/islands/SaveUserData.tsx
@@ -1,10 +1,11 @@
 import * as Hooks from "preact/hooks";
 import { Button } from "../components/Button.tsx";
 
-export default function SaveUserData({ jwt = false, user }) {
-  if (jwt) {
+export default function SaveUserData({ jwt, user }) {
+  Hooks.useLayoutEffect(() => {
     window.localStorage.setItem("jwt", jwt);
     window.localStorage.setItem("user", JSON.stringify(user));
     window.location = "/";
-  }
+  }, []);
+  return <div> Redirecting to home... </div>;
 }
