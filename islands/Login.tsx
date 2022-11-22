1e91468d011b4896426183e0cc3af7f31cc06abd
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-22 18:59:18 +0100

    clearer prompt for full handle with @

--- islands/Login.tsx
+++ islands/Login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
           <input
             class="inline-flex items-center h-16 px-4 w-1/2 border(2 solid blue-400) border-l-0 border-t-0 border-r-0 outline-none text-black"
             onInput={(e) => setLoginId(e.currentTarget.value)}
-            placeholder="What is your mastodon handle?"
+            placeholder="@your-username@your.instance"
           >
           </input>
           <button
