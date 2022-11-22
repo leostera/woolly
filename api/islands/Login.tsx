7328c6356deb712308f889825f3a60a049ba9367
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-22 11:18:16 +0100

    remove dark mode override

--- api/islands/Login.tsx
+++ api/islands/Login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
           class="flex-1 text-center"
         >
           <input
-            class="inline-flex items-center h-16 px-4 w-1/2 border(2 solid blue-400) border-l-0 border-t-0 border-r-0 outline-none text-black dark:text-white"
+            class="inline-flex items-center h-16 px-4 w-1/2 border(2 solid blue-400) border-l-0 border-t-0 border-r-0 outline-none text-black"
             onInput={(e) => setLoginId(e.currentTarget.value)}
             placeholder="What is your mastodon handle?"
           >
