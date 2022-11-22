654be2b6f016a06279728eae14b4917a8c73180b
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-22 11:07:04 +0100

    fix: turn login into a form to support enter-submit (closes #2)

--- api/islands/Login.tsx
+++ api/islands/Login.tsx
@@ -12,26 +12,33 @@ export default function Login() {
   const [disabled, setDisabled] = useState(true);
   useEffect(() => setDisabled(href === "#"), [href]);
 
+  console.log(loginId, href, disabled);
+
   return (
     <section class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-12">
       <div class="md:flex items-center">
-        <div class="flex-1 text-center">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            window.location.href = href;
+          }}
+          class="flex-1 text-center"
+        >
           <input
             class="inline-flex items-center h-16 px-4 w-1/2 border(2 solid blue-400) border-l-0 border-t-0 border-r-0 outline-none text-black dark:text-white"
-            onKeyDown={(e) => setLoginId(e.currentTarget.value)}
+            onInput={(e) => setLoginId(e.currentTarget.value)}
             placeholder="What is your mastodon handle?"
           >
           </input>
-          <a
+          <button
             disabled={disabled}
-            href={href}
             class={`inline-flex items-center h-16 px-8 font-bold border(2 solid blue-400) border-l-0 border-t-0 border-r-0 ${
               disabled ? "text-gray-500" : "text-blue-500 hover:text-blue-300"
             }`}
           >
             Start Writing
-          </a>
-        </div>
+          </button>
+        </form>
       </div>
     </section>
   );
